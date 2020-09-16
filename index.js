67a6bf884e601f0a3bedfba4f389efd11eed8596
Jake Grella <jgrella13@gmail.com>
2020-09-16 15:54:48 -0500

    completed mvp tasks and stretch goals

--- index.js
+++ index.js
@@ -39,9 +39,42 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+// Write a Person Constructor that initializes `name` and `age` from arguments.
+function Person(name, age) {
+  this.name = name
+  this.age = age
+  this.stomach = []
 }
+// Give instances of Person the ability to `.eat("someFood")`
+Person.prototype.eat = function(edible) {
+  if(this.stomach.length < 10) {
+    this.stomach.push(edible)
+  }
+}
+// Give instances of Person the ability to `.poop()`
+Person.prototype.poop = function() {
+  this.stomach = []
+}
+// Give instances of Person a method `.toString()`
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
+}
+
+const personOne = new Person('William', 21)
+const personTwo = new Person('Grady', 33)
+const personThree = new Person('Sally', 41)
+
+console.log(personOne.toString())
+console.log(personTwo.toString())
+console.log(personThree.toString())
+
+personTwo.eat('🍍')
+personTwo.eat('🌽')
+personTwo.eat('🌮')
+
+console.log(personTwo.stomach)
+personTwo.poop()
+console.log(personTwo.stomach)
 
 /*
   TASK 2
@@ -56,9 +89,25 @@ function Person() {
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
-
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
+}
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons
+}
+Car.prototype.drive = function(distance) {
+  if((this.tank * this.milesPerGallon) < distance) {
+    let leftover = distance - (this.tank * this.milesPerGallon)
+    this.odometer += distance - leftover
+    this.tank = 0
+    return `I ran out of fuel at ${this.odometer} miles!}`
+  } else {
+    this.odometer += distance
+    this.tank -= distance / this.milesPerGallon
+  }
 }
 
 /*
@@ -68,18 +117,35 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
+}
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global Binding (Window Binding)
+      - value of this is window or console object
+      - default if no context given, except when using strict where undefined is returned
+  2. Implicit Binding
+      - most common rule, about 80% of the time
+      - only applies to objects with methods
+      - `this` referes to whatever is left of dot
+  3. New Binding
+      - `this` points to `new` object
+      - when a constructor function is used, `this` is referring to the object being created and returned by constructor function
+  4. Explicit
+      - uses
+        - `.call` - will immediately invoke function / pass in arguments one by one
+        - `.apply` - will immediately invoke function / pass in args as array
+        - `.bind` - will NOT immediately invoke function / pass in args one by one
+      - Whenever JavaScript's call or apply method is used, this is explicitly defined.
 */
 
 
